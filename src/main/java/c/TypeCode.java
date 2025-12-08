187e15535e4eec54068ac515dcde58f9f648c6f4
Alexander Lisborg <alexanderlisborg@gmail.com>
2025-12-08 19:39:20 +0100

    Add Enum TypeCode
    Used for comparing types when checking if expressions are type correct.

---
+++ src/main/java/c/TypeCode.java
@@ -0,0 +1,21 @@
+package c;
+
+public enum TypeCode {
+    CVoid,CChar,CShort,CInt,CLong,CFloat,CDouble,CSigned,CUnsigned
+}
+/*
+Tvoid.        Type_specifier ::= "void";
+Tchar.        Type_specifier ::= "char";
+Tshort.       Type_specifier ::= "short";
+Tint.         Type_specifier ::= "int";
+Tlong.        Type_specifier ::= "long";
+Tfloat.       Type_specifier ::= "float";
+Tdouble.      Type_specifier ::= "double";
+Tsigned.      Type_specifier ::= "signed";
+Tunsigned.    Type_specifier ::= "unsigned";
+Tstruct.      Type_specifier ::= Struct_or_union_spec;
+Tenum.        Type_specifier ::= Enum_specifier;
+Tname.        Type_specifier ::= "Typedef_name";
+Struct.   Struct_or_union ::= "struct";
+Union.    Struct_or_union ::= "union";
+* */
